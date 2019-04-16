ec0b8e12a102834edd9bb2faff78d27b7a156fb1
Jeremy Chase <jeremy.chase@gmail.com>
2019-04-16 17:12:44 -0400

    add lorum ipsum Components

--- src/Root.js
+++ src/Root.js
@@ -1,18 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import Types from './Types';
+import AppBar from "@material-ui/core/AppBar";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  appbar: {
+    height: "24px"
+  },
+  content: {},
+  paper: {
+    margin: theme.spacing.unit * 4,
+    padding: theme.spacing.unit * 4
+  },
+  root: {
+    flexGrow: 1
+  },
+  topAccent: {
+    height: "6px",
+    backgroundColor: theme.palette.primary.dark
+  }
+});
 
 class Root extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <React.Fragment>
-        <header>
-          <p>hello, world</p>
-        </header>
-        <Types />
-      </React.Fragment>
+      <div className={classes.root}>
+        <div className={classes.topAccent} />
+        <AppBar className={classes.appbar} position="sticky" />
+
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Typography variant="h3" gutterBottom>
+                Heading
+              </Typography>
+              <Typography variant="body1">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                blanditiis tenetur unde suscipit, quam beatae rerum inventore
+                consectetur, neque doloribus, cupiditate numquam dignissimos
+                laborum fugiat deleniti? Eum quasi quidem quibusdam.
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
 
-export default Root;
+Root.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles, { withTheme: true })(Root);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import 'typeface-roboto';
+import deepPurple from "@material-ui/core/colors/deepPurple";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "typeface-roboto";
 
-import Root from './Root';
+import Root from "./Root";
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  palette: {
+    primary: deepPurple
+  }
+});
 
 function App() {
-    return (
-          <MuiThemeProvider theme={theme}>
-            <CssBaseline />
-            <Root />
-          </MuiThemeProvider>
-        );
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Root />
+    </MuiThemeProvider>
+  );
 }
 
-render(<App />, document.querySelector('#react-app'));
+render(<App />, document.querySelector("#react-app"));
